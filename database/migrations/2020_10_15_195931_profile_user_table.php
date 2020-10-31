523520f660a3c91522ce3dd767c5dacaaa1ea74b
Sane905 <core.editor>
2020-10-31 11:55:15 +0900

    中間テーブルの説明書き消した

--- database/migrations/2020_10_15_195931_profile_user_table.php
+++ database/migrations/2020_10_15_195931_profile_user_table.php
@@ -19,12 +19,14 @@ class ProfileUserTable extends Migration
             $table->unsignedBigInteger('profile_id');
             $table->timestamps();
 
+             // user_idとprofile_idの組み合わせの重複を許さない
+            $table->unique(["profile_id", "user_id"])->name("user_id_profile_id_unique");
+
              // 外部キー制約    
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->foreign('profile_id')->references('id')->on('profiles')->onDelete('cascade');
 　　　　　　　
-            // user_idとprofile_idの組み合わせの重複を許さない
-            $table->unique(['profile_id', 'user_id'])->name('user_id_profile_id_unique');
+           
 
             
         });
