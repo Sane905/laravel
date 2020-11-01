cea8b1645f7d89b73ab170b8d5c8777dca6e3962
Sane905 <core.editor>
2020-11-01 23:46:33 +0900

    Procfile add

--- database/migrations/2020_10_15_195931_profile_user_table.php
+++ database/migrations/2020_10_15_195931_profile_user_table.php
@@ -18,7 +18,7 @@ class ProfileUserTable extends Migration
             $table->unsignedBigInteger('user_id');
             $table->unsignedBigInteger('profile_id');
             $table->timestamps();
-            $table->unique(['user_id', 'profile_id']);
+            $table->unique(['user_id','profile_id']);
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->foreign('profile_id')->references('id')->on('profiles')->onDelete('cascade');
 　　　　});
