fa32dfb2caf0e206dc3c515af5ee6f43796d08d3
Sane905 <core.editor>
2020-11-01 23:42:23 +0900

    Procfile add

--- database/migrations/2020_10_15_195931_profile_user_table.php
+++ database/migrations/2020_10_15_195931_profile_user_table.php
@@ -24,9 +24,7 @@ class ProfileUserTable extends Migration
              // 外部キー制約    
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->foreign('profile_id')->references('id')->on('profiles')->onDelete('cascade');
-　　　　　　　
-            // user_idとprofile_idの組み合わせの重複を許さない
-            });
+　　　　});
     }
 
     /**
