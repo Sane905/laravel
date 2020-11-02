08b63a69e0702d0399c01088c80706e30a93816d
Sane905 <core.editor>
2020-11-02 15:35:54 +0900

    Procfile add

--- app/Http/Controllers/MypageController.php
+++ app/Http/Controllers/MypageController.php
@@ -153,7 +153,7 @@ class MypageController extends Controller
 
     
             if($request->hasFile('image')){
-                Storage::delete('profile_image',$profiles->image,'public');
+                Storage::delete($profiles->image);
                 $url=$request->file('image');
                 $path=Storage::disk('s3')->putFile('profile_image',$url,'public');
                 $profiles->image=Storage::disk('s3')->url($path);
