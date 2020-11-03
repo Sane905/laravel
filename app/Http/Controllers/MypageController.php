9b0be799345d1c9f37120aebe235a0e0199f2701
Sane905 <core.editor>
2020-11-03 13:49:56 +0900

    Procfile add

--- app/Http/Controllers/MypageController.php
+++ app/Http/Controllers/MypageController.php
@@ -22,10 +22,10 @@ class MypageController extends Controller
     public function index()
     {
         $user=Auth::id();
-        $users=Profile::find($user);
+        $users=Profile::find(Auth::id());
         $profile=User::find($user);
 
-        $receives=Profile::find($user)->comment()->orderBy('created_at','desc')->paginate(5);;
+        $receives=$users->comment()->orderBy('created_at','desc')->paginate(5);
         $favorite=User::find($user)->favorites();
         $sends=User::find($user)->comment()->orderBy('created_at', 'desc')->get();
         $favorites=$users->users()->orderBy('created_at', 'desc')->get();
