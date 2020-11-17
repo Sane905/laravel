14f7e67f2a35115c7a0b58fa18c93d6dea966dda
Sane905 <core.editor>
2020-11-17 12:57:35 +0900

    ユーザ

--- app/Http/Controllers/MypageController.php
+++ app/Http/Controllers/MypageController.php
@@ -23,7 +23,7 @@ class MypageController extends Controller
     {
         $user=Auth::id();
         $users=Profile::where('user_id',$user);
-        $receives=$users->comment->orderBy('created_at','desc')->get();
+        $receives=$users->comment()->orderBy('created_at','desc');
         $favorite=User::find($user)->favorites();
         $sends=User::find($user)->comment()->orderBy('created_at','desc')->get();
         $favorites=$users->users()->orderBy('created_at','desc')->get();
