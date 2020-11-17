aee383c3976775b24c0f386503684d5970da1dd2
Sane905 <core.editor>
2020-11-18 01:05:26 +0900

    画像ファイルのプレビュー

--- resources/js/components/FileComponent.vue
+++ resources/js/components/FileComponent.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="file">
-  <input name="image" type="file" accept="image/*" class="form-control-file" id="exampleFormControlFile1" @change="onFileChange($event)">
+  <input name="image" type="file" accept="image/*" v-on:change="onFileChange($event)" class="form-control-file" id="exampleFormControlFile1">
   <img :src="imageData" v-if="imageData">
 
   </div>
