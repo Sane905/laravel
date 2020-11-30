4b9bbb236530d1fd433aa9961a38b002e93ec65f
Sane905 <core.editor>
2020-11-30 12:31:56 +0900

    構文ミスを修正

--- app/Chat.php
+++ app/Chat.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Chat extends Model
 {
-    protected $fillable=['send','recieve','message'];
+    protected $fillable = ['send','recieve','message'];
 
     public function user()
     {
