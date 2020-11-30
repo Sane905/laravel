2093a4358a09b0a5c9c3a465d22f58910d84dd51
Sane905 <core.editor>
2020-11-30 12:34:00 +0900

    構文ミスを修正

--- app/Comment.php
+++ app/Comment.php
@@ -6,13 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    protected $fillable=['user_id','profile_id','body'];
+    protected $fillable = [
+        'user_id','profile_id','body',
+    ];
+
     public function profile()
     {
         return $this->belongsTo('App\Profile','profile_id');
     }
 
-    public function user(){
+    public function user()
+    {
         return $this->belongsTo('App\User','user_id');
     }
 }
