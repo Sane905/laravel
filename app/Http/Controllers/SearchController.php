29ea745cd7b5185f99013822988b9b11ee5ac777
Sane905 <core.editor>
2020-12-01 18:05:06 +0900

    構文ミスを修正

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -9,17 +9,17 @@ class SearchController extends Controller
 {
     public function index(Request $request)
     {
-        $place = $request->place;
-        $age = $request->age;
-        $keyword = $request->keyword;
-        $gender = $request->gender;
-        $music = $request->music;
+        $place=$request->place;
+        $age=$request->age;
+        $keyword=$request->keyword;
+        $gender=$request->gender;
+        $music=$request->music;
 
         
-        $query = Profile::query()->where('user_id','<>',Auth::id());
+        $query=Profile::query()->where('user_id','<>',Auth::id());
 
        if($place==('error')&&empty($keyword)&&$age==('error')&&empty($gender)&&$music==('error')){
-            $profiles = Profile::where('user_id','<>',Auth::id())->paginate(6);
+            $profiles=Profile::where('user_id','<>',Auth::id())->paginate(6);
        }elseif($request->has('keyword')){
             $query->Where('introduce','LIKE',"%{$keyword}%")
             ->orWhere('artist','LIKE',"%{$keyword}%")
@@ -30,20 +30,20 @@ class SearchController extends Controller
             ->orWhereHas('user',function($query)use($keyword){
                 $query->where('name',$keyword);
             });
-            $profiles = $query->paginate(6);
+            $profiles=$query->paginate(6);
         }elseif($request->has('age')&&$age!=('error')&&$request->has('place')&&$place!=('error')&&$request->has('gender')&&$request->has('music')&&$music!=('error')){
         $query->Where('age',$age)
         ->Where('place',$place)
         ->Where('music','LIKE',"%{$music}%")
         ->Where('gender',$gender);
 
-        $profiles = $query->where('user_id' ,'<>' , Auth::id())->paginate(6);
+        $profiles=$query->where('user_id' ,'<>' , Auth::id())->paginate(6);
         }elseif($request->has('age')&&$age!=('error')&&$request->has('place')&&$place!=('error')&&$request->has('gender')){
             $query->Where('age',$age)
                     ->Where('place',$place)
                     ->Where('gender',$gender);
 
-                $profiles = $query->paginate(6);
+                $profiles=$query->paginate(6);
             
 
             
@@ -51,56 +51,56 @@ class SearchController extends Controller
             $query->Where('age',$age)
             ->Where('music','LIKE',"%{$music}%")
             ->Where('gender',$gender);
-            $profiles = $query->paginate(6);
+            $profiles=$query->paginate(6);
         }elseif($request->has('music')&&$music!=('error')&&$request->has('place')&&$place!=('error')&&$age=('error')&&$request->has('gender')){
             $query->Where('place',$place)
             ->Where('music','LIKE',"%{$music}%")
             ->Where('gender',$gender);
-            $profiles = $query->paginate(6);
+            $profiles=$query->paginate(6);
         }elseif($request->has('music')&&$music!=('error')&&$request->has('place')&&$place!=('error')&&$request->has('age')&&$age!=('error')&&empty($gender)){
             $query->Where('place',$place)
             ->Where('music','LIKE',"%{$music}%")
             ->Where('age',$age);
-            $profiles = $query->paginate(6);
+            $profiles=$query->paginate(6);
         }elseif($request->has('music')&&$music!=('error')&&$request->has('place')&&$place!=('error')&&empty($gender)&&$age=('error')){
             $query->Where('place',$place)
             ->Where('music','LIKE',"%{$music}%");
-            $profiles = $query->paginate(6);
+            $profiles=$query->paginate(6);
         }elseif($request->has('age')&&$age!=('error')&&$request->has('music')&&$music!=('error')&&$place=('error')&&empty($gender)){
             $query->Where('age',$age)
             ->Where('music','LIKE',"%{$music}%");
-            $profiles = $query->paginate(6);
+            $profiles=$query->paginate(6);
         }elseif($request->has('gender')&&$request->has('music')&&$music!=('error')&&$place=('error')&&$age=('error')){
                 $query->Where('gender',$gender)
                 ->Where('music','LIKE',"%{$music}%");
-                $profiles = $query->where('user_id' ,'<>' , Auth::id())->paginate(6);
+                $profiles=$query->where('user_id' ,'<>' , Auth::id())->paginate(6);
         }elseif($request->has('age')&&$age!=('error')&&$request->has('gender')&&$place=('error')&&$music==('error')){
                 $query->Where('gender',$gender)
                 ->Where('age',$age);
-                $profiles = $query->paginate(6);
+                $profiles=$query->paginate(6);
         }elseif($request->has('age')&&$age!=('error')&&$request->has('place')&&$place!=('error')&&empty($gender)&&$music==('error')){
             $query->Where('age',$age)
             ->Where('place',$place);
 
-            $profiles = $query->paginate(6);
+            $profiles=$query->paginate(6);
         }elseif($request->has('place')&&$place!=('error')&&$request->has('gender')&&$age=('error')&&$music==('error')){
             $query->Where('gender',$gender)
             ->Where('place',$place);
-            $profiles = $query->paginate(6);
+            $profiles=$query->paginate(6);
         }elseif($request->has('age')&&$age!=('error')&&empty($gender)&&$place=('error')&&$music==('error')){
             $query->Where('age',$age);
-            $profiles = $query->paginate(6);
+            $profiles=$query->paginate(6);
         }elseif($request->has('place')&&$place!=('error')&&empty($gender)&&$age=('error')&&$music==('error')){
             $query->Where('place',$place);
-            $profiles = $query->paginate(6);
+            $profiles=$query->paginate(6);
         }elseif($request->has('gender')&&$age=('error')&&$place=('error')&&$music==('error')){
             $query->Where('gender',$gender);
-            $profiles = $query->paginate(6);
+            $profiles=$query->paginate(6);
         }elseif($request->has('music')&&$music!=('error')&&$age=('error')&&$place=('error')&&empty($gender)){
             $query->Where('music','LIKE',"%{$music}%");
-            $profiles = $query->paginate(6);
+            $profiles=$query->paginate(6);
         }else{
-            $profiles = Profile::where('user_id' ,'<>' , Auth::id())->paginate(6);
+            $profiles=Profile::where('user_id' ,'<>' , Auth::id())->paginate(6);
 
         }
         
