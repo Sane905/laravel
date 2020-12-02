ae2ebb87251fd54b0dd2fdc30e064b1d4e2c11e3
Sane905 <core.editor>
2020-12-03 00:02:15 +0900

    文章変更

--- resources/views/layouts/sub.blade.php
+++ resources/views/layouts/sub.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'オトLABO') }}</title>
+    <title>{{ config('app.name', '音LABO') }}</title>
 
     <!-- Scripts -->
     <script src="{{asset('js/app.js') }}" defer></script>
