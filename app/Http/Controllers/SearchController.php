2ce792f26c483a8eba883b262b0f0d4ae83e33e3
Sane905 <core.editor>
2020-12-05 00:09:49 +0900

    検索エラー修正

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -16,7 +16,7 @@ class SearchController extends Controller
         $music = $request->music;
 
         
-        $profiles = Profile::where('user_id' ,'=' , 1);
+        $profiles = Profile::where('user_id','=' ,Auth::id());
         return view('band.index')->with(['keyword'=>$keyword,'profiles'=>$profiles]);
 
     }
