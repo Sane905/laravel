851c6805584fcba01638da519e4a6358f720c2d4
Sane905 <core.editor>
2020-12-05 00:04:05 +0900

    検索エラー修正

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -16,7 +16,7 @@ class SearchController extends Controller
         $music = $request->music;
 
         
-        $profiles = Profile::where('user_id' ,'=' , 1)->paginate(6);
+        $profiles = Profile::where('user_id' ,'=' , 1);
         return view('band.index')->with(['keyword'=>$keyword,'profiles'=>$profiles]);
 
     }
