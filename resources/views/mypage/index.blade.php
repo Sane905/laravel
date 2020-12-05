206f2f241f8728e7ff19c4797e3102457a2c09ff
Sane905 <core.editor>
2020-12-05 13:33:11 +0900

    ヘッダーに掲示板設置

--- resources/views/mypage/index.blade.php
+++ resources/views/mypage/index.blade.php
@@ -30,7 +30,6 @@
       <div class="col-lg-8 mt-3">
       <a href="mypage/edit" class="btn mypage-btn" role="button" aria-pressed="true"><button type="button" class="btn btn-primary btn-lg active mr-4 text-center">ログイン情報を編集</button></a>
       <a href="mypage/profile" class="btn mypage-btn" role="button" aria-pressed="true"><button type="button" class="btn btn-primary btn-lg active mr-4 text-center">プロフィールを編集</button></a>
-      <a href="message" class="btn mypage-btn" role="button" aria-pressed="true"><button type="button" class="btn btn-primary btn-lg active">オト仲間掲示板</button></a>
 
       
       <div id="app" class="card mt-4">
