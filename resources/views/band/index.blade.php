deb6f50a34eb7bc39151dff37f6808e01701a2bc
Sane905 <core.editor>
2020-12-05 16:31:35 +0900

    検索エラー修正

--- resources/views/band/index.blade.php
+++ resources/views/band/index.blade.php
@@ -8,6 +8,7 @@
   @elseif(empty($keyword))
    <h2>全てのオト仲間を探す</h2>
    @endif
+  <h4 class="ml-5">{{$profiles->total()}}件取得</h4>
    </div>
   
   <div class="row">
@@ -199,7 +200,7 @@
       
     </div>
 
-   
+    
 
 @else
 <div class="card mt-4">
