bd476955c6e43da857a41cc372d4a9d3abcee8bc
Sane905 <core.editor>
2020-12-05 14:13:37 +0900

    検索エラー修正

--- app/Profile.php
+++ app/Profile.php
@@ -28,7 +28,7 @@ class Profile extends Model
     }
 
     public function scopeOpen($query){
-        return $profiles = Profile::where('user_id' ,'=' , 6);
+        return $profiles = Profile::where('user_id' ,'=' , 5);
     }
 
 
