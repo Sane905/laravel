6b6d3da3652e7cfcddb42df97c6dbd6f4c2191ad
Sane905 <core.editor>
2020-12-06 14:45:10 +0900

    検索エラー修正

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -17,21 +17,6 @@ class SearchController extends Controller
 
         $profiles = Profile::open();
 
-        if($request->has('keyword')){
-            $profiles = Profile::keyword($keyword);
-        }elseif($request->has('place')&&$place!=('error')&&empty($gender)&&$age=('error')&&$music==('error')){
-        
-            $profiles = Profile::place($place);
-        }elseif($request->has('gender')&&$age=('error')&&$place=('error')&&$music==('error')){
-            
-            $profiles = Profile::gender($gender);
-        }elseif($request->has('music')&&$music!=('error')&&$age=('error')&&$place=('error')&&empty($gender)){
-
-            $profiles = Profile::music($music);
-        }else{
-            $profiles = Profile::open();
-
-        }
         
         return view('band.index')->with(['keyword'=>$keyword,'profiles'=>$profiles]);
 
