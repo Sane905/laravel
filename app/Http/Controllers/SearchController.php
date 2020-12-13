cb37442cbdb9282d0028f93b45a187fc54dbfe0e
Sane905 <core.editor>
2020-12-13 13:48:32 +0900

    URL修正

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -73,7 +73,7 @@ class SearchController extends Controller
                 $results[] = $profile;
             }
 
-            if(empty($profiles)){
+            if(empty($profile)){
                 $results[] = $profile;
             }
 
