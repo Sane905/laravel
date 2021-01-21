3b0314460d39bdce6159fd24674b32338ec7ef04
Sane905 <konjaku0905@yahoo.co.jp>
2021-01-21 11:11:39 +0900

    ミーティング一覧画面にフラッシュ画面表示

--- resources/views/meetings/index.blade.php
+++ resources/views/meetings/index.blade.php
@@ -2,6 +2,14 @@
 @section('content')
 
 <div class="container mt-4">
+  @if (session('msg_success'))
+        <div class="card mt-3 mb-3 text-center">
+          <div class="card-body">
+          {{ session('msg_success') }}
+          </div>
+        </div>
+
+  @endif
 
     <div class="row">
       <div class="mx-auto col-md-7">
